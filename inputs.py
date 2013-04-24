7331301d87b2ac292cc7f8c890427dac1666747a
YBadiss <yacine.badiss+git@gmail.com>
2013-04-24 16:54:49 +0200

    subtle change for tests

--- inputs.py
+++ inputs.py
@@ -4,23 +4,26 @@ import json
 import os
 
 def ai():
-	return loadJSON("./artists/artist_map.json")
+  return loadJSON("./artists/artist_map.json")
 
-def S():
-	return loadJSON("./radios/radios.json")
+def S(test = True):
+  TEST_RADIOS = [10, 16, 203, 264, 287]
+  return TEST_RADIOS if test else loadJSON("./radios/radios.json")
 
 def ps(S):
-	ret_ps = {}
-	directory = "./radios/radio%d/"
-	for s in S:
-		ret_ps[s] = []
-		for f in os.listdir(directory%(s)):
-			f_content = loadJSON(f) if f.endswith(".json") else None
-			ret_ps[s].extend(f_content if f_content else [])
-	return ret_ps
+  ret_ps = {}
+  directory = "./radios/radio%d/"
+  for s in S:
+    ret_ps[s] = []
+    for filename in os.listdir(directory%(s)):
+      f_content = loadJSON(filename) if filename.endswith(".json") else None
+      if f_content:
+        ret_ps[s].extend(f_content)
+  return ret_ps
 
 def loadJSON(filename):
-	if os.path.exists(filename):
-		with open(filename, "r") as fd:
-			return json.loads(fd.read())
-	return None
+  if os.path.exists(filename):
+    with open(filename, "r") as fd:
+      return json.loads(fd.read())
+  return None
+
